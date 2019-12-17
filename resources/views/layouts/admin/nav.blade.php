6d67c6db11c8e259bff5f3482df33de55e3352a6
MH Khan <majidhkhan@outlook.com>
2019-12-17 21:38:56 +0000

    wip

--- resources/views/layouts/admin/nav.blade.php
+++ resources/views/layouts/admin/nav.blade.php
@@ -1,4 +1,4 @@
-<nav class="main-header navbar navbar-expand navbar-dark navbar-gray-dark border-bottom-0">
+<nav class="main-header navbar navbar-expand navbar-dark navbar-gray-dark fixed-top border-bottom-0">
 
     <ul class="navbar-nav">
         <li class="nav-item">
